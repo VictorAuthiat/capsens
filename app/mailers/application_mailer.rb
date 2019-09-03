19157f522fb1648af368373272858b87fb478727
victor authiat <authiatv@gmail.com>
2019-09-03 10:48:07 +0200

    Move meth(new_message) from AppMailer to PostMailer // Remove template name

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,9 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
   default :to   => 'user@gmail.com',
           :from => 'no-reply@capsens.eu'
-
-  def new_message(from, message)
-    @from, @message = from, message
-    mail(:subject => 'Testing letter_opener_web', :template_name => 'new_message')
-  end
 end
--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -1,2 +1,6 @@
 class PostMailer < ApplicationMailer
+  def new_message(from, message)
+    @from, @message = from, message
+    mail(:subject => 'Testing letter_opener_web')
+  end
 end
