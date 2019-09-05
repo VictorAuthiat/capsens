3b860c8668ca4853909eae4b8b4eac2de466fbe6
victor authiat <authiatv@gmail.com>
2019-09-05 09:23:16 +0200

    edit transaction failure errors => .failure[:error]

--- spec/transactions/create_user_spec.rb
+++ spec/transactions/create_user_spec.rb
@@ -1,22 +1,16 @@
 require 'rails_helper'
 RSpec.describe CreateUser, type: :transactions do
+  subject { CreateUser.new.call(user: user) }
+
   context '#create transaction' do
     let(:user) { create(:user) }
     it 'validate transaction' do
       user.email = nil
       expect(user.save).to eq(false)
     end
-    it 'not receive email' do
-      expect(PostMailer).not_to receive(:new_message)
-      user.save
-    end
     it 'need to receive email' do
-      expect(PostMailer).to receive(:new_message)
-
-      user.save
-    end
-    it 'sends an email' do
-      expect { subject.save }.to change { PostMailer.deliveries.count }.by(1)
+      expect(PostMailer).to receive_message_chain(:new_message, :deliver_now)
+      subject
     end
   end
 end
