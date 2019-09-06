912398849a68de3544d3379d29386af098713cb7
victor authiat <authiatv@gmail.com>
2019-09-06 17:34:34 +0200

    Edit seed + rspec and validation(model project)

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,9 @@ FactoryBot.create_list(:category, 5)
 puts 'Categories created'
 FactoryBot.create_list(:project, 10)
 puts 'Projects created'
-FactoryBot.create_list(:counterpart, 10)
+FactoryBot.create_list(:counterpart, 5)
 puts 'Counterparts created'
 FactoryBot.create_list(:contribution, 30)
 puts 'Contributions created'
 AdminUser.create!(email: 'admin@capsens.eu', password: 'password', password_confirmation: 'password') if Rails.env.development?
+User.create!(email: 'a@a.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
--- spec/factories/counterparts.rb
+++ spec/factories/counterparts.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :counterpart do
     project
+    name { Faker::Creature::Cat.name }
     amount_in_cents { rand(5000...10_000) }
   end
 end
