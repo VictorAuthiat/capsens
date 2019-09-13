a93359841d551d610c81d1140f02ab951c885d71
victor authiat <authiatv@gmail.com>
2019-09-13 11:36:14 +0200

    Add ransack search to project#index

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,7 @@
 class ProjectsController < ApplicationController
   def index
-    @projects = Project.all.where(aasm_state: %w[upgoing ongoing success])
+    @q = Project.all.where(aasm_state: %w[upgoing ongoing success]).ransack(params[:q])
+    @projects = @q.result
   end
 
   def show
