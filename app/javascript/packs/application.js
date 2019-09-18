af0d4c144c30b441f959c34815fffff739187899
victor authiat <authiatv@gmail.com>
2019-09-18 18:01:00 +0200

    add flatpickr / permited params for edit user

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,3 +2,14 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+
+import flatpickr from "flatpickr"
+require("flatpickr/dist/flatpickr.css")
+
+document.addEventListener("turbolinks:load", () => {
+  flatpickr("[data-behavior='flatpickr']", {
+    altInput: true,
+    altFormat: "F j, Y",
+    dateFormat: "Y-m-d",
+  })
+})
